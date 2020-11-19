8026e7fca14c65b2e19b412f28ede88cb1b8a2b2
Barri <info@bdesignstudio.co.uk>
2020-11-19 19:30:40 +0000

    Basic set up express

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# E-commerce website 
+
+* practice javascript and node js
+#
+### express : webserver maker
+### nodemon : CL tool
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi there');
+const express = require('express');
+
+// app is an object that describe everything that webserver can do 
+const app = express();
+
+//route handler -- tells server what to do when in gets network request from browser 
+//req or request have info about user input or request --from user --object
+//res or response have info from server and data  -- from server --object 
+app.get('/', ( req, res )=> {
+  res.send('Hi there!')
+
+});
+
+app.listen(3000, () =>{
+  console.log('Listening');
+});
