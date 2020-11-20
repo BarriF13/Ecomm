10c0fab4c4b5f06e9a7e58175f3da63be15bed5c
Barri <info@bdesignstudio.co.uk>
2020-11-20 13:32:16 +0000

    refactor getAll

--- repositories/users.js
+++ repositories/users.js
@@ -11,30 +11,43 @@ class UserRepository {
     try {
       //If file name exist
       fs.accessSync(this.filename);
-    } catch (err){
+    } catch (err) {
       //if not make one
       fs.writeFileSync(this.filename, '[]')
     }
-   
+
   }
-async getAll(){
-  // Open the file called this.filename
-  const contents = await fs.promises.readFile(this.filename, {encoding:'utf-8'});
-  // Read its content
-  console.log(contents);
-  // Parse the contents
+  // async getAll(){
+  //   // Open the file called this.filename
+  //   const contents = await fs.promises.readFile(this.filename, {encoding:'utf-8'});
+  //   // Read its content
+  //  // console.log(contents);
+  //   // Parse the contents
+  // const data = JSON.parse(contents);
+  //   // Return the parsed data
+  //  return data;
+  // }
+  //--- refactor the getAll
+  async getAll() {
 
-  // Return the parsed data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: 'utf-8' }));
+  }
 }
-}
-
-
 /// tests
-
+//----------------1
 //new UserRepository();
-const test = async () => {
-const repo = new UserRepository('users.json');
+//----------------2
+// const test = async () => {
+// const repo = new UserRepository('users.json');
 
-await repo.getAll();
+// await repo.getAll();
+// };
+//-----------------3
+const test = async () => {
+  const repo = new UserRepository('users.json');
+
+  const users = await repo.getAll();
+  console.log(users);
 };
 test();
