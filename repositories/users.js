591492af2e2fb9c5dc981631b2a98f5fe4af3008
Barri <info@bdesignstudio.co.uk>
2020-11-20 13:24:30 +0000

    read content of  user repos

--- repositories/users.js
+++ repositories/users.js
@@ -9,18 +9,32 @@ class UserRepository {
 
     this.filename = filename;
     try {
-      //If file name 
+      //If file name exist
       fs.accessSync(this.filename);
     } catch (err){
+      //if not make one
       fs.writeFileSync(this.filename, '[]')
     }
    
   }
+async getAll(){
+  // Open the file called this.filename
+  const contents = await fs.promises.readFile(this.filename, {encoding:'utf-8'});
+  // Read its content
+  console.log(contents);
+  // Parse the contents
 
+  // Return the parsed data
+}
 }
 
 
 /// tests
 
 //new UserRepository();
+const test = async () => {
 const repo = new UserRepository('users.json');
+
+await repo.getAll();
+};
+test();
