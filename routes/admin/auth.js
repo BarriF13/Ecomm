397dadb9c81812759d489b33810b39c38b2c3681
Barri <info@bdesignstudio.co.uk>
2020-11-25 13:41:26 +0000

    add custom validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -13,64 +13,75 @@ router.get('/signup', (req, res) => {
 });
 //post request handler
 router.post('/signup',
- [
-//normalize and validate
-  check('email').trim().normalizeEmail().isEmail(),
+  [
+    //normalize and validate
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email: email });
+        if (existingUser) {
+          throw new Error('Email has been already registered')
+        }
+      }),
 
-  check('password').trim().isLength({ min: 4, max: 20}),
+    check('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters'),
 
-  check('passwordConfirmation').trim().isLength({ min: 4, max: 20})
+    check('passwordConfirmation')
+      .trim().isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters')
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error('Passwords must match')
+        }
+      })
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
 
-],async (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  const { email, password, passwordConfirmation } = req.body;
+    if (password !== passwordConfirmation) {
+      return res.send('Passwords do not match')
+    }
 
-  const existingUser = await usersRepo.getOneBy({ email: email });
-  if (existingUser) {
-    return res.send('Email has been already registered')
-  }
+    const user = await usersRepo.create({ email: email, password: password });
 
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords do not match')
-  }
+    req.session.userId = user.id;
+    res.send('Account created!');
 
-  const user = await usersRepo.create({ email: email, password: password });
-
-
-  req.session.userId = user.id;
-  res.send('Account created!');
-
-
-});
+  });
 // get request handler 
 // sign out-- tell the server to forget the cookies
-router.get('/signout', ( req, res)=>{
+router.get('/signout', (req, res) => {
   req.session = null;
   res.send('You are logged out');
 });
 
 //show only sign in form to user--user to server f
-router.get('/signin', (req, res)=>{
+router.get('/signin', (req, res) => {
   res.send(signinTemplate())
 });
 //server side to user
-router.post('/signin', async (req, res)=>{
-  const {email, password} = req.body;
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
 
-  const user = await usersRepo.getOneBy({email: email});
-  if(!user){
+  const user = await usersRepo.getOneBy({ email: email });
+  if (!user) {
     return res.send('Email not found!')
   }
   const validPassword = await usersRepo.comparePassword(
     user.password,
     password
   );
-  if( !validPassword ){
+  if (!validPassword) {
     return res.send('Invalid password')
   }
   req.session.userId = user.id;
   res.send('You are signed in !!!')
 
 })
-module.exports= router;
+module.exports = router;
