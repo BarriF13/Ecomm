d2a4667a8d68babcb7449b319905e4b23435e8a0
Barri <info@bdesignstudio.co.uk>
2020-11-26 18:37:59 +0000

    list the products in the main page

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,12 @@
 // for viewing 
 const express = require('express');
-
+const productsRepo = require('../repositories/products');
+const productsIndexTemplate = require('../views/products/index')
 
 const router = express.Router();
 
-router.get('/' , async(req, res)=>{
-res.send('Products!!!')
+router.get('/', async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
 });
 module.exports = router;
