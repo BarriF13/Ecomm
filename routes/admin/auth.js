d5a4262f2183708e7f747c7128275df8fe5094a5
Barri <info@bdesignstudio.co.uk>
2020-11-26 12:52:21 +0000

    reusable Error handling middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
-
+// const { check, validationResult } = require('express-validator');
+const { handleErrors} = require('../admin/middleware');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -12,6 +12,7 @@ const {
   requireValidPasswordForUser
 } = require('./validators');
 
+
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
@@ -22,16 +23,10 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    //console.log(errors);
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
-
     req.session.userId = user.id;
     res.send('Account created!!!');
 
@@ -51,14 +46,10 @@ router.get('/signin', (req, res) => {
 router.post(
   '/signin',
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    //console.log(errors);
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ errors }));
-    }
-    const { email } = req.body;
 
+    const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
