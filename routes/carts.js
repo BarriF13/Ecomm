3ecd7739697a819fb8ba5a425c576e082c3c051b
Barri <info@bdesignstudio.co.uk>
2020-11-26 19:20:49 +0000

    submit a product to cart

--- routes/carts.js
+++ routes/carts.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 // Receive a post  request from the button to add an item to a cart
 
-
+router.post('/cart/products', (req,res) => {
+  console.log(req.body.productId);
+  res.send('Product added to cart')
+ })
 // Receive a get req to show all the item in a cart 
 
 // Receive a post req to delete an item in the cart 
