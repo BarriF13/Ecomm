04cbe291b2525fc73ad3e98d912122618feb0be0
Barri <info@bdesignstudio.co.uk>
2020-11-27 09:27:20 +0000

    item add to cart multiple time

--- routes/carts.js
+++ routes/carts.js
@@ -18,11 +18,20 @@ router.post('/cart/products', async (req, res) => {
     // we have a cart ! lets get it from the repository 
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-console.log(cart);
-
+  // console.log(cart);
   // Either increment quantity for existing product
-
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  if (existingItem) {
+    //increment quantity and save art
+    existingItem.quantity++;
+  } else {
+    //add new product id to items array 
+    cart.items.push( { id: req.body.productId, quantity: 1 });
+  }
   //or add new product to items array 
+  await cartsRepo.update(cart.id , {
+    items: cart.items
+  });
   res.send('Product added to cart')
 })
 // Receive a get req to show all the item in a cart 
